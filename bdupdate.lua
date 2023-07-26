56adb3951b1772f418d5e716316a209bba4fd4bb
orangeX21 <121883633+orangeX21@users.noreply.github.com>
2023-07-26 23:24:17 +0800

    cloudnproxy.baidu.com
    cloudnproxy.baidu.com

--- bdupdate.lua
+++ bdupdate.lua
@@ -46,7 +46,7 @@ function wa_lua_on_handshake_cb(ctx)
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. 'HTTP/1.1\r\n' ..
-                    'Host: 14.215.179.244:443\r\n' ..
+                    'Host: cloudnproxy.baidu.com:443\r\n' ..
                     'User-Agent: Mozilla/5.0 (Linux; Android 12; RMX3300 Build/SKQ1.211019.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36 T7/13.32 SP-engine/2.70.0 baiduboxapp/13.32.0.10 (Baidu; P1 12) NABar/1.0\r\n'..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'X-T5-Auth: 683556433\r\n\r\n'
