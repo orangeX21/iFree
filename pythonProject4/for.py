62db40454cc255a50b3c4fae7225bfdd6f2400c5
Orange XU <121883633+orangeX21@users.noreply.github.com>
2024-02-18 20:27:31 +0800

    Delete pythonProject4 directory

--- pythonProject4/for.py
+++
@@ -1,6 +0,0 @@
-while 1:
-    name = input("name;")
-    num = input("num")
-    print("""dataset <- dataset %>%
-      mutate_at(vars({}),
-                list(~ ifelse(. %in% c({}), NA, .)))""".format(name, num))
